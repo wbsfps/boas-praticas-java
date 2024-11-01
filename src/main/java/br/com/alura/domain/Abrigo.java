dc0a009be7881e4f4b0f8568476193908671cee5
William Santana <williamandrade1058@gmail.com>
2024-11-01 15:17:30 -0300

    up

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -5,6 +5,7 @@ public class Abrigo {
     private String nome;
     private String telefone;
     private String email;
+    private Pet[] pets;
 
     public Abrigo(){}
 
@@ -28,5 +29,9 @@ public class Abrigo {
 
     public String getEmail() {
         return email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
     }
 }
